1f633bff5c15d0f9edf4140729d3368237a44e95
Husnain <139365289+Niansuh@users.noreply.github.com>
2024-08-05 04:22:54 -0500

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,7 +155,7 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          <a href="https://gpt-chatbot.ru" style="color: #2D35FF; text-decoration: none;">GPT-ChatBot</a>
+          <a href="https://gpt-chatbot.ru">GPT-ChatBot.ru</a>
         </div>
         <div className={styles["sidebar-sub-title"]}>
           AI assistant.
